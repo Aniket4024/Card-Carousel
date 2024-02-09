8967a2415e98a3f7a08f5f6f33d2b24c7fc5de9c
aniketZ-lanzztech <aniket.zade@lanzztech.com>
2024-02-09 18:30:15 +0530

    final

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -1,6 +1,6 @@
 import style from "../CSS/Card.module.css"
 import { IoLogoGithub, IoLogoLinkedin, IoLogoTwitter } from "react-icons/io5";
-
+import Photo from "../AniketZadeProfessional.png"
 
 const Card = () => {
 
@@ -12,7 +12,7 @@ const Card = () => {
         <div
             className={style.cardS1}
             style={{
-                backgroundImage: `url(${"https://www.daksfirst.com/assets/images/amit.jpeg"})`
+                backgroundImage:`url(${Photo})`
             }}
         >
             <div className={style.cardInnerSection}>
@@ -21,7 +21,7 @@ const Card = () => {
                         Sumit comes with capital markets expertise with coverage extending to Europe and the Middle East. He comes with 14+ years’ experience in structured credit, derivatives, corporate finance and asset management with a focus on emerging markets accompanied by a broad institutional network. He worked at JP Morgan and a Buy side boutique PE, a private equity firm in London He is also an investor in Daksfirst and Co-CEO of Maystone Capital a UK focussed merchant bank. He holds a Bachelor and Masters in engineering from IIT Bombay, an MBA from IIM Lucknow and CFA and FRM certifications
                         Sumit comes with capital markets expertise with coverage extending to Europe and the Middle East. He comes with 14+ years’ experience in structured credit, derivatives, corporate finance and asset management with a focus on emerging markets accompanied by a broad institutional network. He worked at JP Morgan and a Buy side boutique PE, a private equity firm in London He is also an investor in Daksfirst and Co-CEO of Maystone Capital a UK focussed merchant bank. He holds a Bachelor and Masters in engineering from IIT Bombay, an MBA from IIM Lucknow and CFA and FRM certifications
                     </p>
-                    <h3>Kartik Krishnaswamy</h3>
+                    <h3>Aniket Zade</h3>
                     <div className={style.socialIcons}>
                         <IoLogoLinkedin className={style.icons} />
                         <IoLogoTwitter className={style.icons} />
@@ -29,7 +29,7 @@ const Card = () => {
                     </div>
                 </div>
                 <div className={style.CIS1}>
-                    <h1>Kartik Krishnaswamy</h1>
+                    <h1>Aniket Zade</h1>
                     {/* <hr /> */}
                     <p>Vice President, Investment Team</p>
                 </div>
--- src/Component/Carousel.jsx
+++ src/Component/Carousel.jsx
@@ -6,9 +6,12 @@ import Card from "./Card";
 const ServicesCarousel = ()=>{
 
 
+    const arr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
 
     return <>
     
+        {/* 4 Card */}
         <Splide 
             aria-label="My Favorite Images" 
             options={ {
@@ -16,7 +19,34 @@ const ServicesCarousel = ()=>{
                 gap   : '0px',
                 type   : 'loop',
                 perPage: 4,
-                perMove: 4,
+                perMove: 3,
+                pagination:false,
+                autoplay:true,
+                speed:700,
+                lazyLoad: 'nearby',
+                arrows: 2,
+                
+            }}
+            id={style.ServicesCarousel}
+        >
+            {
+                arr.map((e)=>{
+                    return <SplideSlide key={e} className={style.Slide}>
+                            <Card/>
+                        </SplideSlide>
+                })
+            }
+        </Splide>
+
+        {/* card 3 */}
+        <Splide 
+            aria-label="My Favorite Images" 
+            options={ {
+                rewind: true,
+                gap   : '0px',
+                type   : 'loop',
+                perPage: 3,
+                perMove: 2,
                 pagination:false,
                 autoplay:true,
                 speed:500,
@@ -24,48 +54,72 @@ const ServicesCarousel = ()=>{
                 arrows: 2,
                 
             }}
-            id={style.ServicesCarousel}
-            
-
+            id={style.ServicesCarousel3}
         >
-            
-            
-            <SplideSlide className={style.Slide}>
-                <Card/>
-            </SplideSlide>
-            <SplideSlide className={style.Slide}>
-                <Card/>
-            </SplideSlide>
-            <SplideSlide className={style.Slide}>
-                <Card/>
-            </SplideSlide>
-            <SplideSlide className={style.Slide}>
-                <Card/>
-            </SplideSlide>
-            <SplideSlide className={style.Slide}>
-                <Card/>
-            </SplideSlide>
-            <SplideSlide className={style.Slide}>
-                <Card/>
-            </SplideSlide>
-            <SplideSlide className={style.Slide}>
-                <Card/>
-            </SplideSlide>
-            <SplideSlide className={style.Slide}>
-                <Card/>
-            </SplideSlide>
-            <SplideSlide className={style.Slide}>
-                <Card/>
-            </SplideSlide>
-            <SplideSlide className={style.Slide}>
-                <Card/>
-            </SplideSlide>
-            <SplideSlide className={style.Slide}>
-                <Card/>
-            </SplideSlide>
-            <SplideSlide className={style.Slide}>
-                <Card/>
-            </SplideSlide>
+            {/* 4 Cards */}
+            {
+                arr.map((e)=>{
+                    return <SplideSlide key={e} className={style.Slide}>
+                            <Card/>
+                        </SplideSlide>
+                })
+            }
+        </Splide>
+
+        {/* card 2 */}
+        <Splide 
+            aria-label="My Favorite Images" 
+            options={ {
+                rewind: true,
+                gap   : '0px',
+                type   : 'loop',
+                perPage: 2,
+                perMove: 1,
+                pagination:false,
+                autoplay:true,
+                speed:500,
+                lazyLoad: 'nearby',
+                arrows: 2,
+                
+            }}
+            id={style.ServicesCarousel2}
+        >
+            {/* 4 Cards */}
+            {
+                arr.map((e)=>{
+                    return <SplideSlide key={e} className={style.Slide}>
+                            <Card/>
+                        </SplideSlide>
+                })
+            }
+        </Splide>
+
+        {/* card 1 */}
+        <Splide 
+            aria-label="My Favorite Images" 
+            options={ {
+                rewind: true,
+                gap   : '0px',
+                type   : 'loop',
+                perPage: 1,
+                perMove: 1,
+                pagination:false,
+                autoplay:true,
+                speed:500,
+                lazyLoad: 'nearby',
+                arrows: 2,
+                
+            }}
+            id={style.ServicesCarousel1}
+        >
+            {/* 4 Cards */}
+            {
+                arr.map((e)=>{
+                    return <SplideSlide key={e} className={style.Slide}>
+                            <Card/>
+                        </SplideSlide>
+                })
+            }
         </Splide>
 
     </>  
